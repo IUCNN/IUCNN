a32d9d01c348924a5d84050813126d3884f44078
azizka <zizka.alexander@gmail.com>
2020-09-13 22:59:25 +0200

    try to fix travis

--- R/convert_IUCN.R
+++ R/convert_IUCN.R
@@ -1,17 +1,35 @@
 #'Format IUCN Red List categories for IUCNN
 
-convert_IUCN <- function(x){
+convert_IUCN <- function(x,
+                         species = "species",
+                         labels = "labels",
+                         accepted_labels = c("CR", "EN", "VU", "NT", "LC"),
+                         level = "detail",
+                         threatend = c("CR", "EN", "VU"),
+                         not_threatened = c("NT", "LC")){
 
-  # accepte either a data.frame, or the output of the iUCN redlist package
-  ##A function to produce the detailed and broad assessments for IUCNN
+  if(is.list(x)){
+    warning("x is list. Assuming input from rredlist")
+
+    dat <- x$result %>%
+      select(species = scientific_name, abels = category)
+  }else{
+    dat <- x %>% select(.data[[species]], .data[[labels]])
+  }
 
   # remove DD and NE
+  out <-  dat %>%
+    filter(.data[[labels]] %in% accepted_labels)
+
+  # if braod convert to broad categories
 
   # convert to numerical
+
 
   # Print summary to screen (e.g. how amny categories and the category coding)
 
   # return output
+  return(out)
 
 }
 
