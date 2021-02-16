3ce06ab5da76bea6e5d0539f033961ec5338fbd7
Tobias Andermann <tobias.andermann@bioenv.gu.se>
2021-02-16 15:06:23 +0100

    cleaned IUCNN_train.py script

--- inst/python/IUCNN_train.py
+++ inst/python/IUCNN_train.py
@@ -59,7 +59,7 @@ def iucnn_train(dataset,
                                      use_bias=use_bias)]
         for i in n_layers[1:]:
             architecture.append(layers.Dense(i, activation=act_f))
-        architecture.append(layers.Dense(1, activation='sigmoid'))    #sigmoid or tanh
+        architecture.append(layers.Dense(1, activation='tanh'))    #sigmoid or tanh
         model = keras.Sequential(architecture)
         optimizer = "adam"       # "adam" or tf.keras.optimizers.RMSprop(0.001)
         model.compile(loss='mean_squared_error',
@@ -82,7 +82,13 @@ def iucnn_train(dataset,
         else:
             rescaled_labels = ((labels/lab_range) +0.5) * (n_labels-1)
         return(rescaled_labels)
-    
+
+    def model_init(mode):
+        if mode == 'regression':
+            model = build_regression_model()
+        elif mode == 'classification':    
+            model = build_classification_model()
+        return model 
     
     if not os.path.exists(path_to_output):
         os.makedirs(path_to_output)
@@ -96,88 +102,96 @@ def iucnn_train(dataset,
         rnd_indx = np.arange(len(labels))
     if rescale_features:
         dataset = (dataset - dataset.min(axis=0)) / (dataset.max(axis=0) - dataset.min(axis=0))
+    
+    
     rnd_dataset = dataset[rnd_indx,:]
-    reordered_labels = labels[rnd_indx]
+    rnd_labels = labels[rnd_indx]
 
     test_size = int(len(labels)*test_fraction)
     train_set = rnd_dataset[:-test_size,:]
     test_set = rnd_dataset[-test_size:,:]
+
+
+    # format labels
+    rnd_labels_cat  = tf.keras.utils.to_categorical(rnd_labels)
+    rnd_labels_scaled = rescaled_labels[rnd_indx]
+    
+    train_labels_cat = rnd_labels_cat[:-test_size,:]
+    train_labels_scaled = rnd_labels_scaled[:-test_size]
+    train_labels = rnd_labels[:-test_size]
+
+    test_labels_cat = rnd_labels_cat[-test_size:,:]
+    test_labels_scaled = rnd_labels_scaled[-test_size:]
+    test_labels = rnd_labels[-test_size:]
+    
+    n_class = rnd_labels_cat.shape[1]
     
     if mode == 'classification':
-        # format labels
-        rnd_labels  = tf.keras.utils.to_categorical(reordered_labels)
-        train_labels_cat = rnd_labels[:-test_size,:]
-        train_labels = reordered_labels[:-test_size]
-        test_labels = rnd_labels[-test_size:,:]
-        n_class = rnd_labels.shape[1]
-
-        model = build_classification_model()
-        model.summary()
-        if patience == 0:
-            # determining optimal number of epochs
-            history_fit = model.fit(train_set, 
-                                train_labels_cat, 
-                                epochs=max_epochs,
-                                validation_split=validation_split, 
-                                verbose=verbose)
-                                
-            best_epoch = np.argmin(history_fit.history["val_loss"])
-            loss_at_best_epoch = history_fit.history["val_loss"][best_epoch]
-            acc_at_best_epoch = history_fit.history["val_accuracy"][best_epoch]
-            model = build_classification_model()
-            model.summary()
-            history = model.fit(train_set, 
-                                train_labels_cat, 
-                                epochs=best_epoch,
-                                validation_split=validation_split, 
-                                verbose=verbose)
-        
-        else:
-            # The patience parameter is the amount of epochs to check for improvement
-            early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
-            history = model.fit(train_set, 
-                                train_labels_cat, 
-                                epochs=max_epochs,
-                                validation_split=validation_split, 
-                                verbose=verbose,
-                                callbacks=[early_stop])
-
-        
-                            
-        test_loss, test_acc = model.evaluate(test_set, 
-                                   test_labels, 
-                                   verbose=verbose)
-        
-        prm_est = model.predict(test_set, verbose=verbose)
-        test_predictions = np.argmax(prm_est, axis=1)
-
-        train_predictions = np.argmax(model.predict(train_set, verbose=verbose), axis=1)
-
+        labels_for_training = train_labels_cat
+        labels_for_testing = test_labels_cat
+        optimize_for_this = "val_loss"
     elif mode == 'regression':
-        # format labels
-        reordered_labels_scaled = rescaled_labels[rnd_indx]
-        train_labels_scaled = reordered_labels_scaled[:-test_size]
-        train_labels = rescale_labels(train_labels_scaled,n_labels,lab_range).astype(int)
-        test_labels_scaled = reordered_labels_scaled[-test_size:]
-        
-        model = build_regression_model()
+        labels_for_training = train_labels_scaled
+        labels_for_testing = test_labels_scaled
+        optimize_for_this = "val_mae"
+
+    # determine stopping point
+    if patience == 0:
+        # determining optimal number of epochs
+        model = model_init(mode)
+        #model.summary()
+        history_fit = model.fit(train_set, 
+                                labels_for_training, 
+                                epochs=max_epochs,
+                                validation_split=validation_split, 
+                                verbose=verbose)
+        stopping_point = np.argmin(history_fit.history[optimize_for_this])
+        # train model
+        model = model_init(mode)
+        model.summary()
+        history = model.fit(train_set, 
+                            labels_for_training, 
+                            epochs=stopping_point,
+                            validation_split=validation_split, 
+                            verbose=verbose)        
+    else:
+        # train model
+        model = model_init(mode)
         model.summary()
         # The patience parameter is the amount of epochs to check for improvement
-        early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
+        early_stop = keras.callbacks.EarlyStopping(monitor=optimize_for_this, patience=patience)
         history = model.fit(train_set, 
-                            train_labels_scaled, 
+                            labels_for_training, 
                             epochs=max_epochs,
                             validation_split=validation_split, 
                             verbose=verbose,
                             callbacks=[early_stop])
-        test_acc,test_predictions = get_regression_accuracy(model,test_set,test_labels_scaled,n_labels,lab_range)
+        
+            
+    if mode == 'classification':
+        test_loss, test_acc = model.evaluate(test_set, 
+                                             labels_for_testing, 
+                                             verbose=verbose)
+        train_predictions = np.argmax(model.predict(train_set, verbose=verbose), axis=1)
+        test_predictions = np.argmax(model.predict(test_set, verbose=verbose), axis=1)
+        train_acc = history.history['accuracy'][-1]
+        val_acc = history.history['val_accuracy'][-1]
+    elif mode == 'regression':
+        test_acc,test_predictions = get_regression_accuracy(model,test_set,labels_for_testing,n_labels,lab_range)
         if return_categorical:
             test_predictions = np.round(test_predictions, 0).astype(int)
+        train_acc, train_predictions = get_regression_accuracy(model,train_set,labels_for_training,n_labels,lab_range)
         test_loss = np.nan
-        train_acc, train_predictions = get_regression_accuracy(model,train_set,train_labels_scaled,n_labels,lab_range)
-        history.history['accuracy'] = [train_acc]
-        history.history['val_accuracy'] = [np.nan]
+        val_acc = np.nan
 
+
+    res = [history.history['loss'][-1],
+           train_acc,
+           history.history['val_loss'][-1],
+           val_acc,
+           test_loss,
+           test_acc]
+    
 
     if plot_labels_against_features:    
         # define figure dimensions
@@ -198,13 +212,14 @@ def iucnn_train(dataset,
     
     if plot_training_stats:
         fig = plt.figure()
-        if mode == 'classification':
+        if mode == 'classification':            
             grid = gridspec.GridSpec(2, 1, wspace=0.2, hspace=0.4)
             fig.add_subplot(grid[0])
             if patience == 0:
+                acc_at_best_epoch = history_fit.history["val_accuracy"][stopping_point]
                 plt.plot(history_fit.history['accuracy'],label='train')
                 plt.plot(history_fit.history['val_accuracy'], label='val')
-                plt.axvline(best_epoch,linestyle='--',color='red')
+                plt.axvline(stopping_point,linestyle='--',color='red')
                 plt.axhline(acc_at_best_epoch,linestyle='--',color='red')
             else:
                 plt.plot(history.history['accuracy'],label='train')
@@ -212,17 +227,26 @@ def iucnn_train(dataset,
             plt.title('Accuracy')
             fig.add_subplot(grid[1])
             if patience == 0:
+                loss_at_best_epoch = history_fit.history["val_loss"][stopping_point]
                 plt.plot(history_fit.history['loss'],label='train')
                 plt.plot(history_fit.history['val_loss'], label='val')
-                plt.axvline(best_epoch,linestyle='--',color='red')
+                plt.axvline(stopping_point,linestyle='--',color='red')
                 plt.axhline(loss_at_best_epoch,linestyle='--',color='red')
             else:
                 plt.plot(history.history['loss'],label='train')
                 plt.plot(history.history['val_loss'], label='val')                
             plt.title('Loss')                
-        elif mode == 'regression':           
-            plt.plot(history.history['mae'],label='train')
-            plt.plot(history.history['val_mae'], label='val')
+        elif mode == 'regression':
+            if patience == 0:
+                mae_at_best_epoch = history_fit.history["val_mae"][stopping_point]
+                plt.plot(history_fit.history['mae'],label='train')
+                plt.plot(history_fit.history['val_mae'], label='val')
+                plt.axvline(stopping_point,linestyle='--',color='red')
+                plt.axhline(mae_at_best_epoch,linestyle='--',color='red')
+            else:
+                plt.plot(history.history['mae'],label='train')
+                plt.plot(history.history['val_mae'], label='val')
+            plt.title('Mean Average Error') 
         plt.legend(loc='lower right')
         fig.savefig(os.path.join(path_to_output,'training_stats.pdf'),bbox_inches='tight')
 
@@ -234,15 +258,9 @@ def iucnn_train(dataset,
 
     # print("\nTest accuracy: {:5.3f}".format(test_acc))
     
-    res = [history.history['loss'][-1],
-           history.history['accuracy'][-1],
-           history.history['val_loss'][-1],
-           history.history['val_accuracy'][-1],
-           test_loss, test_acc]
-    
     model.save( os.path.join(path_to_output, model_name) )
     print("IUC-NN model saved as:", model_name, "in", path_to_output)
-    return [reordered_labels[-test_size:], test_predictions, res]
+    return [test_labels, test_predictions, res]
 
 #plt.scatter(train_labels_scaled,model.predict(train_set).flatten())
 
