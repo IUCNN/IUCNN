38ff05f37b2d092e827658dc2e63719c4a350e34
Alexander Zizka <zizka.alexander@gmail.com>
2021-08-18 15:26:05 +0200

    change docs to pkgdown

--- docs/R/zzz.R
+++
@@ -1,7 +0,0 @@
-
-# Load python modules on load of R package
-.onLoad <- function(libname, pkgname) {
-  # numpy <<- reticulate::import("numpy", delay_load = TRUE)
-  # tensorflow <<- reticulate::import("tensorflow", delay_load = TRUE)
-  reticulate::configure_environment(pkgname)
-}
